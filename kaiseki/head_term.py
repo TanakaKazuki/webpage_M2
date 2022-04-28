e6299608486f92e1647bb70e7f3d772be782a255
TanakaKazuki <kazukiaidjudge@gmail.com>
2022-04-28 17:20:50 +0900

    [Change] change directory

--- kaiseki/head_term.py
+++ kaiseki/head_term.py
@@ -138,7 +138,7 @@ for i in range(len(sitename_list)):
     column_all_term = []
     column_all_term_num = []
     
-    df = pd.read_csv('/Users/kazuki/Desktop/research/data_1619/kaiseki/html_data/correspondence_list_'+sitename+'.csv',header=None)
+    df = pd.read_csv('/Users/kazuki/Desktop/research/data_Research_M2/R_Data_M2/kaiseki/html_data/correspondence_list_'+sitename+'.csv',header=None)
     filename = df[0]
     fileurl = df[1]
     
@@ -149,7 +149,7 @@ for i in range(len(sitename_list)):
         fileurl = df[1][j]
         print('id',filename)
         
-        df_file_head = pd.read_csv('/Users/kazuki/Desktop/research/data_1619/kaiseki/Headline_extraction/head_data/'+sitename+'/head_'+filename+'.csv')
+        df_file_head = pd.read_csv('/Users/kazuki/Desktop/research/data_Research_M2/R_Data_M2/kaiseki/head_data/'+sitename+'/head_'+filename+'.csv')
         #タイトルがあるなら
         #if len(df_file_head['URL']) > 0:
         print(len(df_file_head['URL']))
@@ -201,5 +201,5 @@ for i in range(len(sitename_list)):
     
     #print(df_output.head(3))
     
-    df_output.to_csv('head_data_site/'+sitename+"_header_term.csv",sep=',')
+    df_output.to_csv('/Users/kazuki/Desktop/research/data_Research_M2/R_Data_M2/kaiseki/head_data_site/'+sitename+"_header_term.csv",sep=',')
     
